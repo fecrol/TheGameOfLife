9bc5d4ba3afaa68c70ac6996ea69e206714621cc
Maciej Fec <maciejfec1996@gmail.com>
2022-09-09 20:06:47 +0100

    Add Header, Body and Footer elements

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from "./Header"
+import Body from "./Body"
+import Footer from "./Footer"
 
 export default function Home() {
   return (
     <div className="main-container centre">
-
+        <Header />
+        <Body />
+        <Footer />
     </div>
   )
 }
