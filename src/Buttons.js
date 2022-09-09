cf3771443989be9725f4184dd9fba1b24565fb87
Maciej Fec <maciejfec1996@gmail.com>
2022-09-09 20:04:44 +0100

    initial commit

---
+++ src/Buttons.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Buttons() {
+  return (
+    <div className="buttons-container centre">
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Next</button>
+        <button>Reset</button>
+    </div>
+  )
+}
