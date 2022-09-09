3938291098f665e2e3d00563180169279a0f0f06
Maciej Fec <maciejfec1996@gmail.com>
2022-09-09 20:12:49 +0100

    Add id and class to buttons

--- src/Buttons.js
+++ src/Buttons.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Buttons() {
   return (
     <div className="buttons-container centre">
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Next</button>
-        <button>Reset</button>
+        <button id="start-btn" className="button">Start</button>
+        <button id="stop-btn" className="button">Stop</button>
+        <button id="next-btn" className="button">Next</button>
+        <button id="reset-btn" className="button">Reset</button>
     </div>
   )
 }
