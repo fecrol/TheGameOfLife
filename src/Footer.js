c7c11026e0ef3c0a74870d7575d292518a1f7315
Maciej Fec <maciejfec1996@gmail.com>
2022-09-09 20:07:21 +0100

    Add footer text

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="footer-container">
-
+        <p className="footer-text">Copyright &copy; Maciej Fec 2022</p>
     </div>
   )
 }
