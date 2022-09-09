ce63f55c189763b774dac5e3e97c309281186ef9
Maciej Fec <maciejfec1996@gmail.com>
2022-09-09 20:05:39 +0100

    Add page title

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className="header-container">
-
+        <h1 className="title">The Game of Life</h1>
     </div>
   )
 }
