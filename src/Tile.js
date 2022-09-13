4efd0c7a1a446fbf96ca8ad17b01fdb365dce4fc
Maciej Fec <maciejfec1996@gmail.com>
2022-09-13 11:37:42 +0100

    Add tiles to the screen

--- src/Tile.js
+++ src/Tile.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-export default function Tile() {
+export default function Tile({dimensions}) {
+    
+  const [tilesArr, setTilesArr] = useState([])
+
+  const numOfRows = Math.floor(dimensions.height / 16)
+  const numOfColumns = Math.floor(dimensions.width / 16)
+
+  useEffect(() => {
+    const tiles = generateTiles()
+    setTilesArr([...tiles])
+  }, [numOfColumns])
+  
+  const generateTiles = () => {
+
+    const tiles = []
+    let key = 0
+    
+    for(let y = 0; y < numOfRows; y++) {
+
+      for(let x = 0; x < numOfColumns; x++) {
+        
+        const element = <div key={`tile${key}`} className="tile"></div>
+
+        const tile = {
+          element,
+          x,
+          y,
+          active: false
+        }
+
+        key++
+        tiles.push(tile)
+      }
+    }
+
+    return tiles
+  }
+  
   return (
-    <div></div>
+    tilesArr.map((tile) => {
+      return tile.element
+    })
   )
 }
